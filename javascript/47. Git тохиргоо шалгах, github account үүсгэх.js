be84c88138e77709807c9b9aa85ccbb392d45391
khongor <khongor8832@gmail.com>
2020-08-21 19:43:04 +0800

    2020.08.21

--- javascript/47. Git тохиргоо шалгах, github account үүсгэх.js
+++ javascript/47. Git тохиргоо шалгах, github account үүсгэх.js
@@ -1,1 +1,25 @@
 // learn 47. Git тохиргоо шалгах, github account үүсгэх
+
+/* 
+Git local repository:
+git init 
+git add -A
+git commit -m " "
+git checkout 
+
+Github repository: 
+git remote add origin "git repo url"
+git remote -v 
+git push 
+git pull 
+ 
+Git тохиргоогоо харах: 
+git config --list
+git config --global user.name "NAME"
+git config --global user.email email@address.com
+
+
+Git bash Хамгийн анхны удаа git hud дээр файлаа байршуулахад git hub-ын username, password -ыг оруулахыг шаарддаг. Оруулсаны дараа дахиж асуудаггүй Өөрөөр хэлбэл нэг удаа оруулна гэсэн үг. Гэтэл тэр username, password-аа магадгүй өөр github account -руу хандах шаардлага гач магадгүй. Windows тэрийг санчихсан байдаг. Тэрийг олж өөрчлөх, устгах болбол: 
+Windows дээрх git username, password тохиргоог харах 
+    Control Panel \ User Account\Credential Manager
+*/
