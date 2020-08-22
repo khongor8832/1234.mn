e8554174d54dea6470cc2d1cdad09942f6c93e55
khongor8832 <khongor8832@gmail.com>
2020-08-22 18:22:09 +0800

    2020.08.21

---
+++ javascript/49.Github дээрээс кодоо татаж авах, open source төслүүдээс татаж.js
@@ -0,0 +1,24 @@
+// laern 49.Github дээрээс кодоо татаж авах, open source төслүүдээс татаж.Github
+
+/*
+Github ашиглах дараалал: 
+
+Төслийг татаж авах: 
+    git pull origin master 
+    git clone YOUR-PROJECT-GITHUB-URL
+
+Өөр төслийг татаж авахдаа: 
+    git clone YOUR-PROJECT-GITHUB-URL
+
+Татаж авсан төслийг өөрийн github repo руу хийх: 
+    git remote -v
+    git remote set-url origin YOUR-PROJECT-GITHUB-URL
+    git push origin master 
+
+Өөр хүний code хайх бол google url дээр Github бичээд оронгуутаа search дээр нь хай  
+
+Вэб сайт хийхэд 2 төрлийн хавтас үүсдэг. 
+    Docs - энэ болохоож хүмүүс харагдахаад бүх зүйлийг нь шахаад харуулдаг. Хурдан ашиллахын тулд 
+    Scr - энэ програмистын бичсэн код байж байна. Хүнд харагдахгүй. 
+
+*/
